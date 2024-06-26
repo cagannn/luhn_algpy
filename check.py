b1fa133af0c9125038c41c74c2c3d680f6dc00fe
Çağan <cagangdk6@gmail.com>
2024-06-26 19:46:09 +0300

    Create check.py

---
+++ check.py
@@ -0,0 +1,29 @@
+a=str(input(""))
+card_num=[]
+counter=0
+sumt=0
+tamp=0
+sumc=0
+for i in a:
+    
+    card_num.append(int(i))
+
+for j in  card_num:
+    if counter%2!=0:
+        sumt=sumt+j
+    else:
+        tamp=j*2
+        if tamp>=10:
+            tamp=str(tamp)
+            for l in tamp:
+                sumc=sumc+int(l)
+        else:
+            sumc=sumc+tamp
+    counter+=1
+last_sum=sumc+sumt
+if last_sum%10==0:
+    print("Gercek kredi karti numarasi")
+else:
+    print("Gercek kredi karti numarasi değil")
+
+        
